f2f21df789eb56ee9598479707197c993f0cfbb1
NarelyLima <narely148@gmail.com>
2025-01-23 18:37:44 -0300

    add facade para criacao de ancora

---
+++ HandSuite/Enums/HandPoint.swift
@@ -0,0 +1,18 @@
+//
+//  HandPoint.swift
+//  HandSuite
+//
+//  Created by Narely Lima de Oliveira on 22/01/25.
+//
+
+import Foundation
+
+public extension HandSuiteTools {
+    enum HandPoint {
+        case aboveHand
+        case indexFingerTip
+        case palm
+        case thumbTip
+        case wrist
+    }
+}
---
+++ HandSuite/Facade/AnchorFacade.swift
@@ -0,0 +1,81 @@
+//
+//  AnchorFacade.swift
+//
+//  Created by Narely Lima de Oliveira on 21/01/25.
+//
+
+import RealityKit
+import Vision
+import Combine
+
+public extension HandSuiteTools {
+    
+    class AnchorState: ObservableObject {
+        @Published public var anchorEntity: AnchorEntity?
+        public init() {}
+    }
+    
+    class AnchorFacade {
+        @available(visionOS 2.0, *)
+        @MainActor
+        public static func createAnchor(for point: HandSuiteTools.HandPoint) -> AnchorEntity? {
+            switch point {
+            case .aboveHand:
+                return createAnchorForAboveHand()
+            case .indexFingerTip:
+                return createAnchorForIndexFingerTip()
+            case .palm:
+                return createAnchorForPalmCenter()
+            case .thumbTip:
+                return createAnchorForThumbTip()
+            case .wrist:
+                return createAnchorForWrist()
+            }
+        }
+        @available(visionOS 2.0, *)
+        @MainActor
+        private static func createAnchorForAboveHand() -> AnchorEntity {
+            let anchor = AnchorEntity(.hand(.either, location: .aboveHand), trackingMode: .predicted)
+            addMetallicBall(to: anchor)
+            return anchor
+        }
+        @available(visionOS 2.0, *)
+        @MainActor
+        private static func createAnchorForIndexFingerTip() -> AnchorEntity {
+            let anchor = AnchorEntity(.hand(.either, location: .indexFingerTip), trackingMode: .predicted)
+            addMetallicBall(to: anchor)
+            return anchor
+        }
+        @available(visionOS 2.0, *)
+        @MainActor
+        private static func createAnchorForPalmCenter() -> AnchorEntity {
+            let anchor = AnchorEntity(.hand(.either, location: .palm), trackingMode: .predicted)
+            addMetallicBall(to: anchor)
+            return anchor
+        }
+        @available(visionOS 2.0, *)
+        @MainActor
+        private static func createAnchorForThumbTip() -> AnchorEntity {
+            let anchor = AnchorEntity(.hand(.either, location: .thumbTip), trackingMode: .predicted)
+            addMetallicBall(to: anchor)
+            return anchor
+        }
+        @available(visionOS 2.0, *)
+        @MainActor
+        private static func createAnchorForWrist() -> AnchorEntity {
+            let anchor = AnchorEntity(.hand(.either, location: .wrist), trackingMode: .predicted)
+            addMetallicBall(to: anchor)
+            return anchor
+        }
+        @available(visionOS 2.0, *)
+        @MainActor
+        private static func addMetallicBall(to anchor: AnchorEntity) {
+            let sphere = MeshResource.generateSphere(radius: 0.005)
+            let material = SimpleMaterial(color: .blue, isMetallic: false)
+            let sphereEntity = ModelEntity(mesh: sphere, materials: [material])
+            
+            sphereEntity.position = [0, 0, 0]
+            anchor.addChild(sphereEntity)
+        }
+    }
+}
