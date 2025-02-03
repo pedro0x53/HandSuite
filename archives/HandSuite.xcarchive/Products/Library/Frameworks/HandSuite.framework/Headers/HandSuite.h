8e117be6385d5c97b0e20d038bc4e420b91fe401
Pedro <54442374+pedro0x53@users.noreply.github.com>
2025-02-03 20:31:57 -0300

    archiving the framework binary

---
+++ archives/HandSuite.xcarchive/Products/Library/Frameworks/HandSuite.framework/Headers/HandSuite.h
@@ -0,0 +1,18 @@
+//
+//  HandSuite.h
+//  HandSuite
+//
+//  Created by Pedro Sousa on 07/01/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HandSuite.
+FOUNDATION_EXPORT double HandSuiteVersionNumber;
+
+//! Project version string for HandSuite.
+FOUNDATION_EXPORT const unsigned char HandSuiteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HandSuite/PublicHeader.h>
+
+
